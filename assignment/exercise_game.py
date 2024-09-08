2d180ec7ca81868bf424cca78a2a2e8bce687c75
ajp1206 <adrian@dunbarcircles.com>
2024-09-08 14:14:57 -0400

    Added correct data calculations for exercise 3

--- assignment/exercise_game.py
+++ assignment/exercise_game.py
@@ -8,7 +8,7 @@ import random
 import json
 
 
-N: int = 3
+N: int = 10
 sample_ms = 10.0
 on_ms = 500
 
@@ -51,13 +51,29 @@ def scorer(t: list[int | None]) -> None:
     print(f"You missed the light {misses} / {len(t)} times")
 
     t_good = [x for x in t if x is not None]
-
+    
+    if t_good:
+        min_time = min(t_good)
+        max_time = max(t_good)
+        avg_time = sum(t_good) / len(t_good)
+    else:
+        min_time, max_time, avg_time = None, None, None
+        
+    print(f"Average response time: {avg_time} ms")
+    print(f"Minimum response time: {min_time} ms")
+    print(f"Maximum response time: {max_time} ms")
+    
     print(t_good)
 
     # add key, value to this dict to store the minimum, maximum, average response time
     # and score (non-misses / total flashes) i.e. the score a floating point number
     # is in range [0..1]
-    data = {}
+    data = {"total_flashes": len(t),
+        "misses": misses,
+        "min_response_time": min_time,
+        "max_response_time": max_time,
+        "avg_response_time": avg_time,
+        "score": (len(t_good) / len(t))}
 
     # %% make dynamic filename and write JSON
 
@@ -100,3 +116,4 @@ if __name__ == "__main__":
     blinker(5, led)
 
     scorer(t)
+
